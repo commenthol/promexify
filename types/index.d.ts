2cac1732d9ace510c6aee57dd9d305041b0a2614
commenthol <commenthol@gmail.com>
2021-12-05 08:29:25 +0100

    fix: update types
    chore: exclude configs from final package

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,14 @@
 /**
- * promisify middlewares
+ * promisify connect middlewares
  * @param  {...any} mws
  * @returns {any[]} promisified middlewares
  */
-export function promid(...mws: any[]): any[];
+export function promex(...mws: any[]): any[];
 /**
- * promisify
+ * promisify for express app or router
  * @param {Application|Router} app express application or router
  */
-export function promisify(app: Application | Router): void;
+export function promexify(app: Application | Router): void;
 export type Application = import('express').Application;
 export type Router = import('express').Router;
 export type ErrorRequestHandler = import('express').ErrorRequestHandler;
